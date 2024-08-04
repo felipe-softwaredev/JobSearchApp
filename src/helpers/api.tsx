7136817329ef0598930b7005c10c934b97155117
Felipe Ramos <129113618+fliperamos88@users.noreply.github.com>
2024-08-04 12:42:25 -0400

    update api URL

--- src/helpers/api.tsx
+++ src/helpers/api.tsx
@@ -1,5 +1,5 @@
 export default class JoblyAPI {
-  static baseURL = process.env.NEXT_PUBLIC_VERCEL_URL;
+  static baseURL = `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`;
 
   static async findAll(endpoint: string) {
     const response = await fetch(`${this.baseURL}/${endpoint}`, {
